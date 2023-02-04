7cd41cf5e55265235f20d58382f48dcfccb0aa07
jesse-rb <jesse.reynekebarnard@gmail.com>
2023-02-04 14:56:06 +1300

    Add functions to set loggers local prefix and color without have to call the new logger function and provider both color and prefix

--- slogger.go
+++ slogger.go
@@ -2,6 +2,8 @@ package logger
 
 import (
 	"fmt"
+	"go/constant"
+	"image/color"
 	"io"
 	"log"
 )
@@ -23,6 +25,8 @@ const (
 // Contains a standard golang log.logger for this package to exectute it's functions
 type Logger struct {
     l *log.Logger
+    color string
+    localPrefix string
 }
 
 // Init logger global prefix
@@ -47,7 +51,8 @@ func logGeneral(log *log.Logger, tag string, msg string, data interface{}) {
 
 // Create a new logger similr to how you would create a default go log.Logger with log.New()
 func New(out io.Writer, color string, localPrefix string, flag int) *Logger {
-    return &Logger{ l: log.New(out, formatPrefix(color, localPrefix), flag) }
+    var l *log.Logger = log.New(out, formatPrefix(color, localPrefix), flag)
+    return &Logger{ l: l, color: color }
 }
 
 // Log something, you can provide:
@@ -58,3 +63,12 @@ func (l *Logger) Log(tag string, msg string, data interface{}) {
     logGeneral(l.l, tag, msg, data)
 }
 
+// Set local prefix of logger
+func (l *Logger) SetLocalPrefix(localPrefix string) {
+    l.l.SetPrefix(formatPrefix(l.color, localPrefix))
+}
+
+// Set color of logger
+func (l *Logger) SetColor(color string) {
+    l.l.SetPrefix(formatPrefix(color, l.localPrefix))
+}
